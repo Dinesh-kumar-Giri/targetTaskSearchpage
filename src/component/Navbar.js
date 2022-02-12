426ffbb4077c39f63704a035f1b280a5a7ba4c27
sachinpal8050101 <sachin.kumar1@daffodilsw.com>
2022-02-12 12:44:54 +0530

    till logo & funcationla component & api call

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,79 +1,76 @@
-import React, { Component } from "react";
+import React from 'react'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar    navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-              <a class="navbar-brand" href="#">
-              {/* <img src="logo1.png" alt="" width="30" height="24"/> */}
-              logo
+function Navbar() {
+  return (
+    <div>
+    <nav className="navbar    navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+          <a class="navbar-brand" href="#">
+          <img src={require('../assect/logo.jpeg')} alt="" width="30" height="24"/>
+          </a>
+          <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
               </a>
-              <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                MEN
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                WOMEN
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                HOME & LIVING
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                BEAUTY
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                STUDIO
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit" style={{color: 'azure'}} >
+              Search
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    MEN
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    WOMEN
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    HOME & LIVING
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    BEAUTY
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    STUDIO
-                  </a>
-                </li>
-              </ul>
-              <form className="d-flex">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit" style={{color: 'azure'}} >
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
+          </form>
+        </div>
       </div>
-    );
-  }
+    </nav>
+  </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
